b8f5ac7be1da1edfee038388fa8d79a92fafd771
Tim Taylor <tim@toolbear.io>
2019-02-12 00:10:50 -0800

    name the StaleHollowReferenceDetector monitor thread

--- hollow/src/main/java/com/netflix/hollow/api/client/StaleHollowReferenceDetector.java
+++ hollow/src/main/java/com/netflix/hollow/api/client/StaleHollowReferenceDetector.java
@@ -114,7 +114,7 @@ public class StaleHollowReferenceDetector {
 
     public void startMonitoring() {
         if (monitor == null) {
-            monitor = new Thread(new Monitor(this));
+            monitor = new Thread(new Monitor(this), "hollow-stale-reference-detector");
             monitor.setDaemon(true);
             monitor.start();
         }
